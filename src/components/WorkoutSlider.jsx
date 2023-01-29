89048e531dfbea39a5ee28edc80655f642629cf7
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-29 20:26:46 +0700

    modified: Workouts components

--- src/components/WorkoutSlider.jsx
+++ src/components/WorkoutSlider.jsx
@@ -1,10 +1,62 @@
 import React from "react";
 
+// import swiper components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper modules
+import { Navigation } from "swiper";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "../assets/styles/_workoutSlider.css";
+
+// import workouts data
+import { workouts } from "../data";
+
 const WorkoutSlider = () => {
+  // destructure workouts data
+  const { programs } = workouts;
+
   return (
-    <div>
-      <h1>workout slider</h1>
-    </div>
+    <Swiper
+      slidesPerView={2}
+      spaceBetween={32}
+      navigation={true}
+      breakpoints={{
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      }}
+      modules={[Navigation]}
+      className="workoutSlider"
+    >
+      {programs.map((program, index) => {
+        // destructure program
+        const { image, name } = program;
+
+        return (
+          <SwiperSlide
+            key={index}
+            className="relative max-h-[320px] max-w-[320px]"
+          >
+            <img
+              src={image}
+              alt="program image"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute left-[20px] bottom-[20px] flex h-[26px] items-center rounded-[2px] bg-white px-[14px]">
+              <div className="font-primary text-sm font-semibold text-neutral-500">
+                {name}
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
+// import workouts data
+import { workouts } from "../data";
+
+// import components
+import WorkoutSlider from "../components/WorkoutSlider";
+
 const Workouts = () => {
+  // destructure workouts data
+  const { icon, title } = workouts;
+
   return (
-    <div>
-      <h1>workouts</h1>
-    </div>
+    <section className="workouts section">
+      {/* workouts title */}
+      <div
+        className="section-title-group mx-auto max-w-[540px] px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="workouts icon" />
+        <h2 className="h2 section-title">
+          {title}
+          <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+
+      {/* workouts slide */}
+      <div data-aos="fade-up" data-aos-delay="300">
+        <WorkoutSlider />
+      </div>
+    </section>
   );
 };
 
