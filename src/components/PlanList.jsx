c1848270e35ce16ded98334bb2a294f65932f148
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-29 21:27:19 +0700

    modified: Pricing components

--- src/components/PlanList.jsx
+++ src/components/PlanList.jsx
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsCheckCircleFill } from "react-icons/bs";
 
-const PlanList = () => {
+const PlanList = ({ plans }) => {
+  // index state
+  const [index, setIndex] = useState(0);
+
   return (
-    <div>
-      <h1>plan list</h1>
+    <div className="mx-auto flex max-w-[1280px] flex-col items-center justify-center gap-y-4 lg:flex-row">
+      {plans.map((plan, currentIndex) => {
+        // destructure plan
+        const { name, price, list, delay } = plan;
+
+        return (
+          <div
+            key={currentIndex}
+            className="w-full rounded-sm px-4 md:max-w-[620px] lg:min-h-[550px] lg:max-w-[405px]"
+            onClick={() => setIndex(currentIndex)}
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay={delay}
+          >
+            <div
+              className={`${
+                currentIndex === index
+                  ? "bg-neutral-500 text-white"
+                  : "bg-neutral-400/10 text-neutral-500"
+              } flex items-center justify-center py-[40px] px-[30px] transition duration-150 lg:min-h-[550px]`}
+            >
+              <div className="flex flex-row items-center gap-y-8 gap-x-8 lg:flex-col lg:gap-x-0">
+                {/* pricing: name & price wrapper */}
+                <div>
+                  <div
+                    className={`${
+                      currentIndex === index
+                        ? "bg-white text-neutral-500"
+                        : "bg-neutral-500 text-white"
+                    } mx-auto mb-8 flex h-[26px] min-w-min items-center justify-center px-[14px] font-primary text-sm font-semibold`}
+                  >
+                    {name}
+                  </div>
+
+                  <div className="flex flex-col items-center justify-center text-center font-primary text-[40px] font-extrabold lg:text-[50px]">
+                    <div className="leading-none">
+                      <span className="tracking-[-0.1px]">{price}</span>
+                      <span className="text-[30px] font-extrabold">$</span>
+                    </div>
+                    <span className="text-sm font-medium">/month</span>
+                  </div>
+                </div>
+
+                {/* pricing: list & btns */}
+                <div>
+                  <ul className="mb-8 flex flex-col gap-y-4">
+                    {list.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-center gap-x-[10px]"
+                        >
+                          <BsCheckCircleFill className="text-lg" />
+                          {item.name}
+                        </li>
+                      );
+                    })}
+                  </ul>
+
+                  <button
+                    className={`${
+                      currentIndex === index
+                        ? "bg-white text-neutral-500"
+                        : "border border-neutral-500"
+                    } btn btn-lg rounded-[2px] lg:mx-auto`}
+                  >
+                    Join now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
+// import pricing data
+import { pricing } from "../data";
+
+// import components
+import PlanList from "./PlanList";
+
 const Pricing = () => {
+  // destructure pricing data
+  const { icon, title, plans } = pricing;
   return (
-    <div>
-      <h1>pricing</h1>
-    </div>
+    <section className="pricing section">
+      {/* pricing title */}
+      <div
+        className="section-title-group mx-auto max-w-[540px] px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="pricing icon" />
+        <h2 className="h2 section-title">
+          {title}
+          <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+
+      {/* pricing list */}
+      <div>
+        <PlanList plans={plans} />
+      </div>
+    </section>
   );
 };
 
