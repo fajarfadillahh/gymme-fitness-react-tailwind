d17bc6c54033fbff74d61cdc7b548ff3fb6be2d2
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-30 09:58:33 +0700

    modified: Community components

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 
+// import community data
+import { community } from "../data";
+
+// import components
+import CommunitySlider from "./CommunitySlider";
+
 const Community = () => {
+  // destructure community data
+  const { icon, title, testimonials } = community;
+
   return (
-    <div>
-      <h1>community</h1>
-    </div>
+    <section className="community section relative">
+      <div className="container mx-auto">
+        <div className="flex">
+          {/* community title */}
+          <div
+            className="section-title-group mx-auto max-w-[240px] px-4 lg:ml-0 lg:px-0"
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="200"
+          >
+            <img src={icon} alt="community icon" />
+            <h2 className="h2 section-title">
+              {title}
+              <span className="text-primary-200">.</span>
+            </h2>
+          </div>
+
+          {/* community slider */}
+          <div
+            className="absolute -right-[375px] top-48 w-[1140px] lg:top-0 lg:-right-[280px]"
+            data-aos="fade-left"
+            data-aos-offset="200"
+            data-aos-delay="300"
+          >
+            <CommunitySlider testimonials={testimonials} />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/CommunitySlider.jsx
+++ src/components/CommunitySlider.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 
-const CommunitySlider = () => {
+// import swiper components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper modules
+import { Navigation } from "swiper";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+const CommunitySlider = ({ testimonials }) => {
   return (
-    <div>
-      <h1>community slider</h1>
-    </div>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={32}
+      centeredSlides={"auto"}
+      grabCursor={true}
+      modules={[Navigation]}
+    >
+      {testimonials.map((testimonial, index) => {
+        // destructure testimonial
+        const { image, name, message } = testimonial;
+
+        return (
+          <SwiperSlide key={index}>
+            <div className="relative">
+              {/* testimonial img */}
+              <div>
+                <img src={image} alt="testimonial img" />
+              </div>
+
+              {/* testimonial message & name */}
+              <div className="absolute bottom-[30px] p-[20px] text-center text-white">
+                <div className="mb-8 text-lg font-light italic">{message}</div>
+                <div className="flex items-center justify-center gap-x-1">
+                  <span className="text-[30px] font-bold text-primary-200">
+                    ~
+                  </span>
+                  <div className="text-[20px] font-bold">{name}</div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
