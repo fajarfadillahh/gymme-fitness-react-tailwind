9a3eb107f7966f5943db71ed4a75356f56176104
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-30 10:37:46 +0700

    modified: Faq components

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaChevronCircleUp, FaChevronCircleDown } from "react-icons/fa";
 
-const Accordion = () => {
+const Accordion = ({ accordion }) => {
+  // accordion state
+  const [isOpen, setIsOpen] = useState(false);
+
+  // destructure accordion data
+  const { question, answer } = accordion;
+
   return (
-    <div>
-      <h1>accordion</h1>
+    <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+      <div className="rounded-sm border bg-white">
+        {/* accordion question */}
+        <div className="flex min-h-[60px] items-center justify-between px-[30px]">
+          <h6 className="h6">{question}</h6>
+          <div className="text-[20px] text-neutral-500">
+            {isOpen ? <FaChevronCircleUp /> : <FaChevronCircleDown />}
+          </div>
+        </div>
+
+        {/* accordion answer */}
+        <div
+          className={`${
+            isOpen ? "min-h-[200px] lg:min-h-[160px]" : "min-h-0"
+          } flex max-h-0 justify-center overflow-hidden px-[30px] transition-all`}
+        >
+          <div className="mt-6">{answer}</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 
+// import faq data
+import { faq } from "../data";
+
+// import components
+import Accordion from "../components/Accordion";
+
 const Faq = () => {
+  // destructure faq data
+  const { icon, title, accordions } = faq;
   return (
-    <div>
-      <h1>faq</h1>
-    </div>
+    <section className="faq section pt-[480px] lg:pt-[280px]">
+      <div className="mx-auto max-w-[768px] bg-custom bg-center bg-no-repeat lg:h-[1160px] lg:bg-faq lg:pt-6">
+        {/* faq title */}
+        <div
+          className="section-title-group mx-auto justify-start -space-x-4 px-4 lg:max-w-[540px] lg:justify-center lg:px-0"
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-delay="200"
+        >
+          <img src={icon} alt="faq icon" className="lg:hidden" />
+          <h2 className="h2 section-title lg:mt-[100px]">
+            {title}
+            <span className="text-primary-200">.</span>
+          </h2>
+        </div>
+
+        {/* faq accordion list */}
+        <div
+          className="flex flex-col gap-y-4 px-4"
+          data-aos="fade-up"
+          data-aos-offset="300"
+          data-aos-delay="200"
+        >
+          {accordions.map((accordion, index) => {
+            return <Accordion key={index} accordion={accordion} />;
+          })}
+        </div>
+      </div>
+    </section>
   );
 };
 
